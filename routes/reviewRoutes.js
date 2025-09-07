683891465110be50012c198b78d514f556b5b08e
MohamedShawky4 <mohamedshawki4041@gmail.com>
2025-09-07 22:14:33 +0300

    Add review controllers and routes

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const {
+  CreateReview,
+  getAllReviews,
+  getReviewsForService,
+  getReviewById,
+  getReviewsByUser,
+  updateReview,
+  deleteReview,
+  getReviewStats,
+
+} = require("../Controllers/reviewController");
+
+// Create a review
+router.post("/", CreateReview);
+
+// Get all reviews
+router.get("/", getAllReviews);
+
+// Get reviews for a service (trip/hotel/car)
+router.get("/:serviceType/:serviceId", getReviewsForService);
+
+// Get review stats (average + total)
+router.get("/:serviceType/:serviceId/stats", getReviewStats);
+
+// Get a single review by id
+router.get("/id/:id", getReviewById);
+
+// Get reviews by user
+router.get("/user/:userId", getReviewsByUser);
+
+// Update a review
+router.put("/:id", updateReview);
+
+// Delete a review
+router.delete("/:id", deleteReview);
+
+
+module.exports = router;
